cda49b44285ad6c845df86da1e505576e94b008d
Orange <capnorange@protonmail.com>
2020-08-17 01:10:26 +0530

    hooks for repo

--- src/git_server/mod.rs
+++ src/git_server/mod.rs
@@ -1,22 +1,29 @@
 mod hooks;
 mod parser;
 
+use std::fs;
+use std::path::PathBuf;
+
 pub struct GitServerConfig {
-    pub dir: String,
+    pub dir: PathBuf,
     pub git_path: String,
     pub hooks: hooks::HookScripts,
 }
 impl Default for GitServerConfig {
     fn default() -> Self {
         GitServerConfig {
-            dir: "./".into(),
+            dir: "./data/repo".into(),
             git_path: "/usr/bin/git".into(),
-            hooks: hooks::HookScripts {
-                pre_receive: r#"echo "pre-receive""#.into(),
-                update: r#"echo "update""#.into(),
-                post_receive: r#"echo "post-receive""#.into(),
-            },
+            hooks: hooks::HookScripts::default(),
         }
+    }
+}
+
+impl GitServerConfig {
+    pub fn setup(&self) -> Result<(), String> {
+        // Create the repo directory
+        fs::create_dir_all(&self.dir).map_err(|_| "Could not create git repo directory")?;
+        Ok(())
     }
 }
 
